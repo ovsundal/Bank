a7334dde54fee51f1342a6bf49317f5b1ff851c8
ovsundal <osundal83@gmail.com>
2017-11-19 13:05:06 +0100

    feat: add bean MiniBank

---
+++ OpenEJB/ejbModule/sessionsBeans/MiniBankLocal.java
@@ -0,0 +1,12 @@
+package sessionsBeans;
+
+import javax.ejb.Local;
+
+import entity.Account;
+
+@Local
+public interface MiniBankLocal {
+
+	public void deposit(Account acc, int amount);
+	public String withdraw(Account acc, int amount);
+}
