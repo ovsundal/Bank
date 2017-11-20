b23e9e0854ed8d0ba18b34a64f8ff6539d145d25
ovsundal <osundal83@gmail.com>
2017-11-20 20:36:31 +0100

    feat: add minibank-bean transfer money method

--- OpenEJB/ejbModule/sessionsBeans/MiniBankRemote.java
+++ OpenEJB/ejbModule/sessionsBeans/MiniBankRemote.java
@@ -10,4 +10,5 @@ public interface MiniBankRemote {
 	public String deposit(int accountId, int amount);
 	public String withdraw(int accountId, int amount);
 	public String getAccountBalance(int accountId);
+	public String transfer(int fromAccId, int toAccId, int amount);
 }
