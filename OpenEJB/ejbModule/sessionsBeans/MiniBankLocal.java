16e1b55c96246064c1ac899757a524375e56457d
ovsundal <osundal83@gmail.com>
2017-11-23 16:25:36 +0100

    feat: generate data to use for app demo

--- OpenEJB/ejbModule/sessionsBeans/MiniBankLocal.java
+++ OpenEJB/ejbModule/sessionsBeans/MiniBankLocal.java
@@ -10,10 +10,25 @@ import entity.Account;
 public interface MiniBankLocal {
 
 	public String deposit(int accountId, int amount);
+
 	public String withdraw(int accountId, int amount);
+
 	public String getAccountBalance(int accountId);
+
 	public String transfer(int fromAccId, int toAccId, int amount);
+
 	public String createAccount(String personId, String accountName, int initialBalance, Date dateCreated);
+
 	public boolean validateCardNumber(String cardNumber);
+
 	public boolean validatePin(String cardNumber, String pin);
+
+	// only used for generating test data with older log date
+	public String deposit(int accountId, int amount, Date date);
+	
+	// only used for generating test data with older log date
+	public String withdraw(int accountId, int amount, Date date);
+	
+	// only used for generating test data with older log date
+	public String transfer(int fromAccId, int toAccId, int amount, Date date);
 }
