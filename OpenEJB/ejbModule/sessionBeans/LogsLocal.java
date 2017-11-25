d32e563d99326da1c16c5f726aa24042892368e1
ovsundal <osundal83@gmail.com>
2017-11-25 15:14:52 +0100

    refactor: clean and tidy Logs-bean

--- OpenEJB/ejbModule/sessionBeans/LogsLocal.java
+++ OpenEJB/ejbModule/sessionBeans/LogsLocal.java
@@ -10,9 +10,34 @@ import entity.Person;
 
 @Local
 public interface LogsLocal {
-
+	/**
+	 * Adds a log to db
+	 * 
+	 * @param l
+	 */
 	public void add(Log l);
-	public List<Log> list() throws Exception;
-	public void remove(Log l) throws Exception;
+
+	/**
+	 * Lists all logs belonging to an account
+	 * 
+	 * @param accountId
+	 * @return
+	 */
 	public List<Log> showLog(int accountId);
+
+	/**
+	 * Lists all logs in db. Only used for testing
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	public List<Log> list() throws Exception;
+
+	/**
+	 * Removes a log from db. Only used for testing
+	 * 
+	 * @param l
+	 * @throws Exception
+	 */
+	public void remove(Log l) throws Exception;
 }
